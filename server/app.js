852d3c9eb5559fcf7d333d0514b4d878d85387b3
Alayna Buysse <alaynagrace@comcast.net>
2017-05-12 11:42:59 -0500

    server side change

--- server/app.js
+++ server/app.js
@@ -11,9 +11,13 @@ app.get('/info', function(req, res) {
     res.send("hello from the server");
 });
 
-app.get('/', function(req, res) {
+//this MUST BE THE LAST ROUTE
+//OTHERWISE IT'LL ONLY GET THIS
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, './public/views/index.html'));
 });
+
+//can't have server side routes that match client side routes
 
 var port = process.env.PORT || 3000;
 
