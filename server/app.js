7b91786557d52f9329dd54ce5dc846261c7c1b85
Alayna Buysse <alaynagrace@comcast.net>
2017-05-12 09:49:13 -0500

    make it better

--- server/app.js
+++ server/app.js
@@ -15,6 +15,8 @@ app.use('/', function(req, res) {
   res.sendFile(path.join(__dirname, './public/views/index.html'));
 });
 
-app.listen(3000, function() {
-  console.log("server running, check localhost:3000");
+var port = 3000 || process.env.PORT;
+
+app.listen(port, function() {
+  console.log("server running, check", port);
 });
