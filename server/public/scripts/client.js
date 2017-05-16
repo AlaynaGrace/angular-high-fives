7d8afc2b45412b55ccb89ce1d29853fc3514f4a7
Alayna Buysse <alaynagrace@comcast.net>
2017-05-16 12:01:34 -0500

    ngAnimate works and commented code

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,71 +1,64 @@
-var myApp = angular.module('myApp',['ngRoute']);
- myApp.controller('HighFives', function(GetHighFives){
-   //this controller is no longer used
-   var vm = this;
+//Creat angular app
+var myApp = angular.module('myApp',['ngRoute','ngAnimate']);
 
-  //  vm.skillLevel = GetHighFives.getSkillLevel();
-   vm.successful = GetHighFives.getSuccessful();
-   vm.total = GetHighFives.getTotal();
-  //  vm.highFiveMe = function(){
-  //    vm.quality = GetHighFives.randomNumber(1,10);
-  //    GetHighFives.highFives(vm.skillLevel, vm.quality);
-  //    vm.successful = GetHighFives.getSuccessful();
-  //    vm.total = GetHighFives.getTotal();
-  //  };
- });
-
-
+//config is used to config the app
+//in this case, I am using it to config the routes
 myApp.config(function($routeProvider, $locationProvider){
 
    $routeProvider.when('/',{
      templateUrl: 'views/pages/mainTemplate.html',
-     controller: 'HighFives'
+     //doesn't need a controller because I'm not controlling anything
    }).when('/barry',{
      templateUrl: 'views/pages/template.html',
-     controller: 'Barry'
+     controller: 'BarryCtrl as bc'
    }).when('/lauren',{
      templateUrl: 'views/pages/template.html',
-     controller: 'Lauren'
+     controller: 'LaurenCtrl as lc'
    }).when('/olly',{
      templateUrl: 'views/pages/template.html',
-     controller: 'Olly'
+     controller: 'OllyCtrl as oc'
    }).otherwise('/');
 
-   $locationProvider.html5Mode(true);
+   $locationProvider.html5Mode(true); //makes it so we don't need '#!' in the urls
  });
 
- myApp.controller('Barry', function($scope, $route, GetHighFives){
-
-   $scope.highFiveBuddy = {
+ //all animal controllers look the same besides skill of animal
+ myApp.controller('BarryCtrl', function($scope, GetHighFives){
+   var vm = $scope;
+   //This is where the template gets all the info
+   //each animal has this object
+   vm.highFiveBuddy = {
      imageLink: 'http://images1.fanpop.com/images/photos/2500000/hi-5-high-five-2524598-1600-1200.jpg',
      description: 'This is Barry, the high fiving bear! He loves to give high fives!',
      name: 'Barry'
    };
 
-   var vm = $scope;
-
-   GetHighFives.setSkillLevel(1,10);
-   vm.skillLevel = GetHighFives.getSkillLevel();
-   vm.successful = GetHighFives.getSuccessful();
-   vm.total = GetHighFives.getTotal();
+   GetHighFives.setSkillLevel(1,10); //sets the skill level of the animal
+   vm.skillLevel = GetHighFives.getSkillLevel(); //binds that skill level with a variable and shows it on the dom
+   vm.successful = GetHighFives.getSuccessful(); //shows the # of total successful high fives
+   vm.total = GetHighFives.getTotal(); //shows total # of high fives
+   //function to "run" the high five and see if its successful
    vm.highFiveMe = function(){
-     vm.quality = GetHighFives.randomNumber(1,10);
-     GetHighFives.highFives(vm.skillLevel, vm.quality);
+     vm.quality = GetHighFives.randomNumber(1,10); //gets the quality of the high five
+     GetHighFives.highFives(vm.skillLevel, vm.quality); //tests the high five to see if its good
+     //next two update the # of successful high fives and the total # of high fives
+     //this way we know immediately if the high five was successful or not
      vm.successful = GetHighFives.getSuccessful();
      vm.total = GetHighFives.getTotal();
    };
 
  });
 
- myApp.controller('Lauren', function($scope, $route, GetHighFives){
-   $scope.highFiveBuddy = {
+ myApp.controller('LaurenCtrl', function($scope, GetHighFives){
+   var vm = $scope;
+
+   vm.highFiveBuddy = {
      imageLink: 'https://s-media-cache-ak0.pinimg.com/originals/69/98/1d/69981d5d61448c208fbad3b8c86c7499.jpg',
      description: 'Lauren the lioness loves laying around! She also loves high fives!',
      name: 'Lauren'
    };
-   var vm = $scope;
 
-   GetHighFives.setSkillLevel(4,10);
+   GetHighFives.setSkillLevel(4,10); //higher skill level
    vm.skillLevel = GetHighFives.getSkillLevel();
    vm.successful = GetHighFives.getSuccessful();
    vm.total = GetHighFives.getTotal();
@@ -77,15 +70,16 @@ myApp.config(function($routeProvider, $locationProvider){
    };
  });
 
- myApp.controller('Olly', function($scope, $route, GetHighFives){
-   $scope.highFiveBuddy = {
+ myApp.controller('OllyCtrl', function($scope, GetHighFives){
+   var vm = $scope;
+
+   vm.highFiveBuddy = {
      imageLink: 'https://gladallover.files.wordpress.com/2013/01/sea_otter1.jpg?w=646',
      description: 'Olly the otter is the master of swimming and high fives!',
      name: 'Olly'
    };
-   var vm = $scope;
 
-   GetHighFives.setSkillLevel(6,10);
+   GetHighFives.setSkillLevel(6,10); //higher skill level
    vm.successful = GetHighFives.getSuccessful();
    vm.skillLevel = GetHighFives.getSkillLevel();
    vm.total = GetHighFives.getTotal();
--- server/public/scripts/getHighFives-service.js
+++ server/public/scripts/getHighFives-service.js
@@ -1,30 +1,42 @@
 myApp.service('GetHighFives', function(){
+  //global service vars
   var skillLevel = 0;
   var successfulHighFives = 0;
   var total = 0;
 
+  //returns the skill level so we can access it in the controller
   this.getSkillLevel = function(){
     return skillLevel;
   };
 
+  //sets a new skill level
+  //takes in a max and a min so different animals can have different skill levels
   this.setSkillLevel = function(max, min){
     skillLevel = this.randomNumber(max,min);
   };
 
+  //this returns the number of successful high fives
   this.getSuccessful = function(){
     return successfulHighFives;
   };
 
+  //this returns the total number of high fives attempted
   this.getTotal = function(){
     return total;
   };
 
+  //random number function found on the internet
+  //used in the controller to get a random quality
+  //could have a getQuality function
   this.randomNumber = function(max,min){
     return Math.floor(Math.random() * (max - min)) + min;
   };
 
+  //this function tests the high five to see if is successful or not
+  //takes in a skill level and a quality of high five
   this.highFives = function(skill,qualityOfHighFive){
-    total++;
+    total++; //total is incremented regardless of if the high five is successful
+    //if the quality of the high five is greater than or equal to the skill then it is a successful high five
     if(qualityOfHighFive>=skill){
       successfulHighFives++;
     }
